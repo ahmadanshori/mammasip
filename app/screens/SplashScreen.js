26f74c9058257fce766a0b2db195466d12b46ceb
Ahmad Anshori <ahmadanshorri@gmail.com>
2022-05-07 20:01:57 +0700

    update version 12

--- app/screens/SplashScreen.js
+++ app/screens/SplashScreen.js
@@ -31,7 +31,7 @@ const SplashScreen = () => {
       />
       <View style={styles.text}>
         <Text style={[FONTS.textBold12, {color: COLORS.white}]}>
-          Version 1.10
+          Version 1.11
         </Text>
       </View>
     </Container>
